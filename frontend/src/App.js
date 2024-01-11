d0bcca90f73c6cd402de793f24f1b10b8e6e555a
Vishal Giri <vishalgiri197@gmail.com>
2024-01-11 09:58:00 +0530

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 // import GroupComponentSet from './components/GroupComponentSet';
 import Maincontent from './component/Maincontent';
-import Property1Variant2 from './component/Property1Variant2';
+// import Property1Variant2 from './component/Property1Variant2';
 
 function App() {
   return (
     <div >
 <Maincontent/>
-<Property1Variant2/>
+{/* <Property1Variant2/> */}
     </div>
   );
 }
